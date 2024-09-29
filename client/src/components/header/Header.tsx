4658632f080884d7e08ae42c410762a2f19eb128
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-09-29 23:41:02 +0200

    feat: add Header component with styles

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import Divider from '../divider/Divider';
+import './Header.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 className='header__heading'>Synonym Search Tool</h1>
+            <Divider />
+        </div>
+    );
+}
+
+export default Header;
