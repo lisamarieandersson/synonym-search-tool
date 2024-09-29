148fbd28feada8f5969b2cd32ebd873a0d90989a
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-09-29 23:46:32 +0200

    tests: update tests for App and Header component

--- client/tests/Header.test.tsx
+++ client/tests/Header.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
-import App from '../src/App';
+import Header from '../src/components/header/Header';
 
 describe('Header', () => {
     test('should render the correct headline in header', () => {
-        render(<App />);
+        render(<Header />);
         expect(screen.getByText('Synonym Search Tool')).toBeInTheDocument();
     });
 });
