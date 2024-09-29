23f48c7ea76e266196910ed1755504cdffea844d
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-09-29 10:37:31 +0200

    chore: Update project configurations and auto-generated files

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcbb5cbcaa2d2f20ed6005894942356957db6259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed04486647dfea5a3d7abb0959722f376f807a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
