bc849c9a240f9d5e962b723eda233641ec31b65f
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-10-01 14:39:44 +0200

    refactor: cleaned up App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,10 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.scss';
-import AddWordWithSynonymForm from './components/add-word-and-synonym-form/AddWordWithSynonymForm';
 import Header from './components/header/Header';
 import SearchForm from './components/search-form/SearchForm';
-import SynonymResultsList from './components/synonym-results-list/SynonymResultsList';
-import { useSynonymSearch } from './hooks/useSynonymSearch';
 import './styles/index.scss';
 
 const App = () => {
-  const {
-    synonyms,
-    message,
-    searchedWord,
-    handleAddWordWithSynonym,
-  } = useSynonymSearch('');
-
   return (
     <Router>
       <div className="app">
@@ -35,13 +25,8 @@ const App = () => {
           />
         </Routes>
 
-        {/* SynonymResultsList is rendered in the App so it updates live */}
-        {synonyms.length > 0 && (
-          <SynonymResultsList synonyms={synonyms} searchedWord={searchedWord} />
-        )}
-
         {/* Always render the AddWordAndSynonymForm */}
-        <AddWordWithSynonymForm handleAddWordWithSynonym={handleAddWordWithSynonym} message={message} />
+        {/*         <AddWordWithSynonymForm handleAddWordWithSynonym={handleAddWordWithSynonym} message={message} /> */}
         <footer></footer>
       </div>
     </Router>
