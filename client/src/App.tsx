cd43d457a08f85216e78d9b7063c4bcf85e7848e
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-10-01 12:16:23 +0200

    refactor: move components to App.tsx for better separation of concerns

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,51 @@
-import {
-    Navigate,
-    Route,
-    BrowserRouter as Router,
-    Routes,
-} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.scss';
+import AddWordWithSynonymForm from './components/add-word-and-synonym-form/AddWordWithSynonymForm';
 import Header from './components/header/Header';
 import SearchForm from './components/search-form/SearchForm';
+import SynonymResultsList from './components/synonym-results-list/SynonymResultsList';
+import { useSynonymSearch } from './hooks/useSynonymSearch';
 import './styles/index.scss';
 
 const App = () => {
-    return (
-        <Router>
-            <div className="app">
-                <header>
-                    <Header />
-                </header>
+  const {
+    synonyms,
+    message,
+    searchedWord,
+    handleAddWordWithSynonym,
+  } = useSynonymSearch('');
 
-                <Routes>
-                    {/* Redirect from "/" to "/search" */}
-                    <Route path="/" element={<Navigate to="/search" />} />
+  return (
+    <Router>
+      <div className="app">
+        <header>
+          <Header />
+        </header>
 
-                    {/* Search page with query parameter */}
-                    <Route path="/search" element={<SearchForm />} />
-                </Routes>
 
-                <footer></footer>
-            </div>
-        </Router>
-    );
+        <Routes>
+          {/* Render the SearchForm on both the main page and /search */}
+          <Route
+            path="/"
+            element={<SearchForm />}
+          />
+          <Route
+            path="/search"
+            element={<SearchForm />}
+          />
+        </Routes>
+
+        {/* SynonymResultsList is rendered in the App so it updates live */}
+        {synonyms.length > 0 && (
+          <SynonymResultsList synonyms={synonyms} searchedWord={searchedWord} />
+        )}
+
+        {/* Always render the AddWordAndSynonymForm */}
+        <AddWordWithSynonymForm handleAddWordWithSynonym={handleAddWordWithSynonym} message={message} />
+        <footer></footer>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
