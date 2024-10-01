c58d3778a57fe16742d4f455ffae6b5ffdd48e4e
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-10-01 16:01:02 +0200

    feat: add handleAddWordWithSynonym function to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.scss';
+import AddWordWithSynonymForm from './components/add-word-and-synonym-form/AddWordWithSynonymForm';
 import Header from './components/header/Header';
 import SearchForm from './components/search-form/SearchForm';
+import { useSynonymSearch } from './hooks/useSynonymSearch';
 import './styles/index.scss';
 
-const App = () => {
+function App() {
+  const { handleAddWordWithSynonym, message } = useSynonymSearch('');
+
   return (
     <Router>
       <div className="app">
@@ -26,7 +30,7 @@ const App = () => {
         </Routes>
 
         {/* Always render the AddWordAndSynonymForm */}
-        {/*         <AddWordWithSynonymForm handleAddWordWithSynonym={handleAddWordWithSynonym} message={message} /> */}
+        <AddWordWithSynonymForm handleAddWordWithSynonym={handleAddWordWithSynonym} message={message} />
         <footer></footer>
       </div>
     </Router>
